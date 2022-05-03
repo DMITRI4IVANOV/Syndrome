22495fdb48571d22e470c7272a037a1076e8c078
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-05-03 20:56:49 +0400

    Initial commit

---
+++ src/main/java/StatisticSindrome/StatSindrom.java
@@ -0,0 +1,19 @@
+package StatisticSindrome;
+
+public class StatSindrom {
+//    // Количество квадратов
+//    public int calcSqrt(int min, int max) {
+//        int count = 0;
+//        for (int i = 10; i <= 99; i++) {
+//            int sqrt = i * i;
+//            if (sqrt < min) {
+//                continue;
+//            }
+//            if (sqrt > max) {
+//                continue;
+//            }
+//            count ++;
+//        }
+//        return count;
+//    }
+}
---
+++ src/test/java/StatisticSindrome/StatSindromTest.java
@@ -0,0 +1,12 @@
+package StatisticSindrome;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatSindromTest {
+
+    @Test
+    void calcSqrt() {
+    }
+}
